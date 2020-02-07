dc91abe74796e5bfdb9a33d1e2bab8c54e086be7
joqtan18 <joqtan.jr@gmail.com>
2020-02-07 10:01:26 -0500

    plantilla lista

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/prueba', function () {
+Route::get('/', function () {
     return view('plantilla.plantilla');
 });
