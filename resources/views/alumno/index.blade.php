2c4282e8edc63d251f6e0a30f6b6f772dd6c4f90
jorgejlrr <jorge14.jlrr@gmail.com>
2020-02-16 20:56:36 -0500

    Correcciones

---
+++ resources/views/alumno/index.blade.php
@@ -0,0 +1,41 @@
+@extends('plantilla.plantilla')
+@section('contenido')
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-header">
+                <i class=""> Alumnos</i>
+            </div>
+            <div class="card-body">
+                <table class="table table-responsive-sm table-hover table-sm">
+                    <thead>
+                        <tr>
+                            <th>DNI</th>
+                            <th>Apellidos</th>
+                            <th>Nombres</th>
+                            <th>Grado</th>
+                            <th>Opciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                            <td>5</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                            <td>5</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
