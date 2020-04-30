e5d58239fc5c5f46d1f8f896d779612c47da0265
joqtan18 <joqtan.jr@gmail.com>
2020-04-30 00:55:45 -0500

    pagos condicional

--- resources/views/pagos/index.blade.php
+++ resources/views/pagos/index.blade.php
@@ -49,16 +49,76 @@
                     			<td>{{'s/'.$d->montoanual}}</td>
                     			<td>{{'s/'.$d->descuento}}</td>
                                 <td>{{'s/'.$d->inicial}}</td>
-                    			<td>{{'s/'.$d->marzo}}</td>
-                    			<td>{{'s/'.$d->abril}}</td>
-                    			<td>{{'s/'.$d->mayo}}</td>
-                    			<td>{{'s/'.$d->junio}}</td>
-                    			<td>{{'s/'.$d->julio}}</td>
-                    			<td>{{'s/'.$d->agosto}}</td>
-                    			<td>{{'s/'.$d->setiembre}}</td>
-                    			<td>{{'s/'.$d->octubre}}</td>
-                    			<td>{{'s/'.$d->noviembre}}</td>
-                    			<td>{{'s/'.$d->diciembre}}</td>
+                    			<td>
+                                    @if ($d->marzo >= 0)
+                                        <span class="badge badge-success">{{'s/'.$d->marzo}}</span>
+                                     @elseif($d->marzo < 0)
+                                        <span class="badge badge-danger">{{'s/'.$d->marzo}}</span>
+                                    @endif        
+                                </td>
+                    			<td>
+                                    @if ($d->abril >= 0)
+                                        <span class="badge badge-success">{{'s/'.$d->abril}}</span>
+                                     @elseif($d->abril < 0)
+                                        <span class="badge badge-danger">{{'s/'.$d->abril}}</span>
+                                    @endif                                     
+                                </td>
+                    			<td>
+                                    @if ($d->mayo >= 0)
+                                        <span class="badge badge-success">{{'s/'.$d->mayo}}</span>
+                                     @elseif($d->mayo < 0)
+                                        <span class="badge badge-danger">{{'s/'.$d->mayo}}</span>
+                                    @endif 
+                                </td>
+                    			<td>
+                                    @if ($d->junio >= 0)
+                                        <span class="badge badge-success">{{'s/'.$d->junio}}</span>
+                                     @elseif($d->junio < 0)
+                                        <span class="badge badge-danger">{{'s/'.$d->junio}}</span>
+                                    @endif          
+                                </td>
+                    			<td>
+                                    @if ($d->julio >= 0)
+                                        <span class="badge badge-success">{{'s/'.$d->julio}}</span>
+                                     @elseif($d->julio < 0)
+                                        <span class="badge badge-danger">{{'s/'.$d->julio}}</span>
+                                    @endif          
+                                </td>
+                    			<td>
+                                    @if ($d->agosto >= 0)
+                                        <span class="badge badge-success">{{'s/'.$d->agosto}}</span>
+                                     @elseif($d->agosto < 0)
+                                        <span class="badge badge-danger">{{'s/'.$d->agosto}}</span>
+                                    @endif          
+                                </td>
+                    			<td>
+                                    @if ($d->setiembre >= 0)
+                                        <span class="badge badge-success">{{'s/'.$d->setiembre}}</span>
+                                     @elseif($d->setiembre < 0)
+                                        <span class="badge badge-danger">{{'s/'.$d->setiembre}}</span>
+                                    @endif          
+                                </td>
+                    			<td>
+                                    @if ($d->octubre >= 0)
+                                        <span class="badge badge-success">{{'s/'.$d->octubre}}</span>
+                                     @elseif($d->octubre < 0)
+                                        <span class="badge badge-danger">{{'s/'.$d->octubre}}</span>
+                                    @endif          
+                                </td>
+                    			<td>
+                                    @if ($d->noviembre >= 0)
+                                        <span class="badge badge-success">{{'s/'.$d->noviembre}}</span>
+                                     @elseif($d->noviembre < 0)
+                                        <span class="badge badge-danger">{{'s/'.$d->noviembre}}</span>
+                                    @endif          
+                                </td>
+                    			<td>
+                                    @if ($d->diciembre >= 0)
+                                        <span class="badge badge-success">{{'s/'.$d->diciembre}}</span>
+                                     @elseif($d->diciembre < 0)
+                                        <span class="badge badge-danger">{{'s/'.$d->diciembre}}</span>
+                                    @endif          
+                                </td>
                     			<td>
                     				<a href="{{url('pago/'.$d->id.'/edit')}}" class="btn btn-sm btn-warning"><i class="fa fa-pencil"></i></a>
                     			</td>
