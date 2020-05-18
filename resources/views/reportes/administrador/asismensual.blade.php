8d8c0670d24fe7e7fdfc64be2237adeb46134d04
joqtan18 <joqtan.jr@gmail.com>
2020-05-17 22:49:11 -0500

    validacion fecha

--- resources/views/reportes/administrador/asismensual.blade.php
+++ resources/views/reportes/administrador/asismensual.blade.php
@@ -7,17 +7,17 @@
                 <strong>Reporte mensual de asistencias</strong>
             </div>
             <div class="card-body">
-            	<form method="post" action="{{url('recebirreporteasismensual')}}">
+            	<form method="post" action="{{url('recebirreporteasismensual')}}" id="elForm">
 					@method('POST')
 					{{ csrf_field() }}
 					<div class="form-group row">
                         <label class="col-md-1 col-form-label">F. Inicio</label>
                         <div class="col-md-3">
-                            <input class="form-control" required type="date" name="finicio" >
+                            <input class="form-control" type="date" name="finicio" id="infechaini" onChange="sinDomingos();" onblur="obtenerfechafinf1();" required >
                         </div>
                         <label class="col-md-1 col-form-label">F. Fin</label>
                         <div class="col-md-3">
-                            <input class="form-control" required type="date" name="ffin" >
+                            <input class="form-control" type="date" name="ffin"  >
                         </div>
                     </div> 
                     <div class="form-group row">
@@ -31,7 +31,7 @@
                         </div>
                     </div>
 					<div class="form-actions">
-                        <input type="submit" value="Generar Reporte" class="btn btn-primary">
+                        <input type="submit" value="Generar Reporte" id="elSubmit" class="btn btn-primary">
                         <a href="{{url('home')}}" class="btn btn-danger">Cancelar</a>
                     </div> 
 				</form>
@@ -39,4 +39,30 @@
         </div>
     </div>
 </div>
+@endsection
+
+@section('scripts')
+<script type="text/javascript">
+    
+var elDate = document.getElementById('infechaini');
+var elForm = document.getElementById('elForm');
+var elSubmit = document.getElementById('elSubmit');
+
+function sinDomingos(){
+    var day = new Date(elDate.value ).getUTCDay();
+    // Días 0-6, 0 es Domingo 6 es Sábado
+    elDate.setCustomValidity(''); // limpiarlo para evitar pisar el fecha inválida
+    if( day == 0 || day == 6 ){
+       elDate.setCustomValidity('por favor seleccione otro día de Lunes a Viernes');
+    } else {
+       elDate.setCustomValidity('');
+    }
+    if(!elForm.checkValidity()) {elSubmit.click()};
+}
+
+function obtenerfechafinf1(){
+    sinDomingos();
+}
+
+</script>
 @endsection
--- resources/views/reportes/administrador/asistencia.blade.php
+++ resources/views/reportes/administrador/asistencia.blade.php
@@ -7,13 +7,13 @@
                 <strong>Reporte diario de asistencias</strong>
             </div>
             <div class="card-body">
-            	<form method="post" action="{{url('recebirreporteasis')}}">
+            	<form method="post" action="{{url('recebirreporteasis')}}" id="elForm">
 					@method('POST')
 					{{ csrf_field() }}
 					<div class="form-group row">
                         <label class="col-md-1 col-form-label">Fecha</label>
                         <div class="col-md-3">
-                            <input class="form-control" required type="date" name="asis_fecha" >
+                            <input class="form-control" type="date" name="asis_fecha" id="infechaini" onChange="sinDomingos();" onblur="obtenerfechafinf1();" required>
                         </div>
                         <label class="col-md-1 col-form-label">Curso</label>
                         <div class="col-md-3">
@@ -25,7 +25,7 @@
                         </div>
                     </div> 
 					<div class="form-actions">
-                        <input type="submit" value="Generar Reporte" class="btn btn-primary">
+                        <input type="submit" value="Generar Reporte" id="elSubmit" class="btn btn-primary">
                         <a href="{{url('home')}}" class="btn btn-danger">Cancelar</a>
                     </div> 
 				</form>
@@ -33,4 +33,30 @@
         </div>
     </div>
 </div>
+@endsection
+
+@section('scripts')
+<script type="text/javascript">
+    
+var elDate = document.getElementById('infechaini');
+var elForm = document.getElementById('elForm');
+var elSubmit = document.getElementById('elSubmit');
+
+function sinDomingos(){
+    var day = new Date(elDate.value ).getUTCDay();
+    // Días 0-6, 0 es Domingo 6 es Sábado
+    elDate.setCustomValidity(''); // limpiarlo para evitar pisar el fecha inválida
+    if( day == 0 || day == 6 ){
+       elDate.setCustomValidity('por favor seleccione otro día de Lunes a Viernes');
+    } else {
+       elDate.setCustomValidity('');
+    }
+    if(!elForm.checkValidity()) {elSubmit.click()};
+}
+
+function obtenerfechafinf1(){
+    sinDomingos();
+}
+
+</script>
 @endsection
