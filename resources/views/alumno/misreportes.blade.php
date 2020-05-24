3e06cb57589e258740dd4868b08d77ef0c45d9c6
joqtan18 <joqtan.jr@gmail.com>
2020-05-24 18:36:12 -0500

    graficos notas

--- resources/views/alumno/misreportes.blade.php
+++ resources/views/alumno/misreportes.blade.php
@@ -47,9 +47,35 @@
                 </div>
               </div>             
 
+              <div class="card">
+                <div class="card-header">Graficos de Notas
+                  <div class="card-header-actions">
+                    <a class="card-header-action" href="http://www.chartjs.org" target="_blank">
+                      <small class="text-muted">docs</small>
+                    </a>
+                  </div>
+                </div>
+                <div class="card-body">
+                  <div class="chart-wrapper">
+                    <canvas id="graficodenotas3"></canvas>
+                  </div>
+                </div>
+              </div>   
 
-
-
+              <div class="card">
+                <div class="card-header">Graficos de Notas
+                  <div class="card-header-actions">
+                    <a class="card-header-action" href="http://www.chartjs.org" target="_blank">
+                      <small class="text-muted">docs</small>
+                    </a>
+                  </div>
+                </div>
+                <div class="card-body">
+                  <div class="chart-wrapper">
+                    <canvas id="graficodenotas2"></canvas>
+                  </div>
+                </div>
+              </div> 
 
             </div>
           </div>
@@ -140,7 +166,7 @@ var lineChart = new Chart($('#graficodenotas'), {
           <?php } ?>
             ],
     datasets: [{
-      label: 'Bimestre I',
+      label: 'I Bimestre',
       backgroundColor: 'rgba(123, 159, 255 , 0.2)',
       borderColor: 'rgba(123, 159, 255 , 1)',
       pointBackgroundColor: 'rgba(123, 159, 255  , 1)',
@@ -150,7 +176,7 @@ var lineChart = new Chart($('#graficodenotas'), {
             '<?php echo $d->not_promedio; ?>',
             <?php } ?> ]
     }, {
-      label: 'Bimestre II',
+      label: 'II Bimestre ',
       backgroundColor: 'rgba(255, 123, 123, 0.2)',
       borderColor: 'rgba(255, 123, 123, 1)',
       pointBackgroundColor: 'rgba(255, 123, 123, 1)',
@@ -160,7 +186,7 @@ var lineChart = new Chart($('#graficodenotas'), {
             '<?php echo $d->not_promedio; ?>',
             <?php } ?>]
     },{
-      label: 'Bimestre III',
+      label: 'III Bimestre',
       backgroundColor: 'rgba(98, 255, 212, 0.2)',
       borderColor: 'rgba(98, 255, 212, 1)',
       pointBackgroundColor: 'rgba(98, 255, 212, 1)',
@@ -170,7 +196,7 @@ var lineChart = new Chart($('#graficodenotas'), {
             '<?php echo $d->not_promedio; ?>',
             <?php } ?>]
     },{
-      label: 'Bimestre IV',
+      label: 'IVBimestre',
       backgroundColor: 'rgba(255, 230, 92, 0.2)',
       borderColor: 'rgba(255, 230, 92, 1)',
       pointBackgroundColor: 'rgba(255, 230, 92, 1)',
@@ -204,7 +230,7 @@ var lineChart = new Chart($('#graficodenotas'), {
         ticks: {
           beginAtZero: true,
                    max: 20,
-                   stepSize: 1,
+                   stepSize: 0,
                    fontSize: 11
         }
       }],
@@ -213,4 +239,145 @@ var lineChart = new Chart($('#graficodenotas'), {
 
 }); // eslint-disable-next-line no-unused-vars
 </script>
+
+<script type="text/javascript">
+  var radarChart = new Chart($('#graficodenotas2'), {
+  type: 'radar',
+  data: {
+    labels: [<?php 
+          foreach ($fechas as  $fecha) { ?>
+            '<?php echo $fecha->asig_nom; ?>',
+          <?php } ?>
+          ],
+    datasets: [{
+      label: 'I Bimestre',
+      backgroundColor: 'rgba(123, 159, 255 , 0.2)',
+      borderColor: 'rgba(123, 159, 255 , 1)',
+      pointBackgroundColor: 'rgba(123, 159, 255  , 1)',
+      pointBorderColor: 'rgba(123, 159, 255 , 1)',
+      data: [<?php 
+          foreach ($notas1 as  $d) { ?>
+            '<?php echo $d->not_promedio; ?>',
+            <?php } ?> ]
+    }, {
+      label: 'II Bimestre',
+      backgroundColor: 'rgba(255, 123, 123, 0.2)',
+      borderColor: 'rgba(255, 123, 123, 1)',
+      pointBackgroundColor: 'rgba(255, 123, 123, 1)',
+      pointBorderColor: 'rgba(255, 123, 123, 1)',
+      data: [<?php 
+          foreach ($notas2 as  $d) { ?>
+            '<?php echo $d->not_promedio; ?>',
+            <?php } ?>]
+    },{
+      label: 'III Bimestre',
+      backgroundColor: 'rgba(98, 255, 212, 0.2)',
+      borderColor: 'rgba(98, 255, 212, 1)',
+      pointBackgroundColor: 'rgba(98, 255, 212, 1)',
+      pointBorderColor: 'rgba(98, 255, 212, 1)',
+      data: [<?php 
+          foreach ($notas3 as  $d) { ?>
+            '<?php echo $d->not_promedio; ?>',
+            <?php } ?>]
+    },{
+      label: 'IV Bimestre',
+      backgroundColor: 'rgba(255, 230, 92, 0.2)',
+      borderColor: 'rgba(255, 230, 92, 1)',
+      pointBackgroundColor: 'rgba(255, 230, 92, 1)',
+      pointBorderColor: 'rgba(255, 230, 92, 1)',
+      data: [<?php 
+          foreach ($notas4 as  $d) { ?>
+            '<?php echo $d->not_promedio; ?>',
+            <?php } ?>]
+    }]
+  },
+  options: {
+    responsive: true
+  }
+}); // eslint-disable-next-line no-unused-vars
+</script>
+
+<script type="text/javascript">
+  var radarChart = new Chart($('#graficodenotas3'), {
+  type: 'bar',
+  data: {
+    labels: [<?php 
+          foreach ($fechas as  $fecha) { ?>
+            '<?php echo $fecha->asig_nom; ?>',
+          <?php } ?>
+          ],
+    datasets: [{
+      label: 'I Bimestre',
+      backgroundColor: 'rgba(123, 159, 255 , 0.2)',
+      borderColor: 'rgba(123, 159, 255 , 1)',
+      pointBackgroundColor: 'rgba(123, 159, 255  , 1)',
+      pointBorderColor: 'rgba(123, 159, 255 , 1)',
+      data: [<?php 
+          foreach ($notas1 as  $d) { ?>
+            '<?php echo $d->not_promedio; ?>',
+            <?php } ?> ]
+    }, {
+      label: 'II Bimestre',
+      backgroundColor: 'rgba(255, 123, 123, 0.2)',
+      borderColor: 'rgba(255, 123, 123, 1)',
+      pointBackgroundColor: 'rgba(255, 123, 123, 1)',
+      pointBorderColor: 'rgba(255, 123, 123, 1)',
+      data: [<?php 
+          foreach ($notas2 as  $d) { ?>
+            '<?php echo $d->not_promedio; ?>',
+            <?php } ?>]
+    },{
+      label: 'III Bimestre',
+      backgroundColor: 'rgba(98, 255, 212, 0.2)',
+      borderColor: 'rgba(98, 255, 212, 1)',
+      pointBackgroundColor: 'rgba(98, 255, 212, 1)',
+      pointBorderColor: 'rgba(98, 255, 212, 1)',
+      data: [<?php 
+          foreach ($notas3 as  $d) { ?>
+            '<?php echo $d->not_promedio; ?>',
+            <?php } ?>]
+    },{
+      label: 'IV Bimestre',
+      backgroundColor: 'rgba(255, 230, 92, 0.2)',
+      borderColor: 'rgba(255, 230, 92, 1)',
+      pointBackgroundColor: 'rgba(255, 230, 92, 1)',
+      pointBorderColor: 'rgba(255, 230, 92, 1)',
+      data: [<?php 
+          foreach ($notas4 as  $d) { ?>
+            '<?php echo $d->not_promedio; ?>',
+            <?php } ?>]
+    }]
+  },
+  options: {
+
+
+
+    scales: {
+
+      xAxes: [{
+        ticks: {
+
+          beginAtZero: true,
+                   max: 100,
+                   stepSize: 0,
+                   fontSize: 8,
+                   maxRotation: 90,
+                   minRotation: 10
+
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+                   max: 20,
+                   stepSize: 0,
+                   fontSize: 11
+        }
+      }],
+    }
+  }
+}); // eslint-disable-next-line no-unused-vars
+</script>
+
+
 @endsection
